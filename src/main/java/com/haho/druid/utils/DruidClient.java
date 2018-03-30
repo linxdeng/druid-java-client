2c32c2faf1ee8fcd4319c0cf81199497da106b2d
denglinxiang <denglinxiang@163.com>
2018-03-30 11:53:11 +0800

    修复httpclent在请求时可能会不受时间限制的占用线程。解决方式是添加超时时间。

--- src/main/java/com/haho/druid/utils/DruidClient.java
+++ src/main/java/com/haho/druid/utils/DruidClient.java
@@ -4,10 +4,12 @@ package com.haho.druid.utils;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.StatusLine;
+import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.utils.HttpClientUtils;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -37,19 +39,27 @@ public class DruidClient {
 
     // 初始化http工具
     private void initHttp() {
-        httpClient = HttpClients.createDefault();
+        HttpClientBuilder httpClientBuilder = HttpClientBuilder.create();
+        // 设置线程数
+        httpClientBuilder.setMaxConnTotal(200);
+        httpClientBuilder.setMaxConnPerRoute(20);
+        httpClient = httpClientBuilder.build();
     }
 
     // 发送post请求
     private DruidResponse post(String url, String context) {
         DruidResponse druidResponse = new DruidResponse();
         logger.debug("druid send message:host={},context={}", url, context);
+        HttpResponse response = null;
         try {
             HttpPost postRequest = new HttpPost(url);
             postRequest.setHeader("Content-type", "application/json");
+            // 设置超时时间，默认5s
+            RequestConfig config = RequestConfig.custom().setConnectTimeout(10000).setConnectionRequestTimeout(10000).setSocketTimeout(5000).build();
+            postRequest.setConfig(config);
             StringEntity message = new StringEntity(context);
             postRequest.setEntity(message);
-            HttpResponse response = httpClient.execute(postRequest);
+            response = httpClient.execute(postRequest);
             StatusLine status = response.getStatusLine();
             if (status.getStatusCode() == 200) {
                 HttpEntity entity = response.getEntity();
@@ -62,6 +72,9 @@ public class DruidClient {
         } catch (Exception e) {
             logger.error("druid请求异常:", e);
             druidResponse.error("系统错误:" + e.getMessage() + e.getStackTrace());
+        } finally {
+            // 关闭无效连接
+            HttpClientUtils.closeQuietly(response);
         }
         return druidResponse;
     }
